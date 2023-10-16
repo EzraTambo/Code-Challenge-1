8d7b4186313bd242251bf345358e05ce3c32db45
+ <+>
2023-10-16 12:15:09 +0300

    final commit 1

---
+++ marks.js
@@ -0,0 +1,20 @@
+let prompt = require('prompt-sync')();
+//get grades
+let input = prompt('Enter Grades:')
+let enteredGrade = parseFloat(input)
+
+//check results and assign Grade
+function gradeGenerator(y){
+
+    if(y> 79 && y<101) return "A"
+    else if (60<= y &&  y<= 79) return "B"
+    else if (50<= y &&  y<= 59) return "C"
+    else if (40<= y &&  y<= 49) return "D"
+    else if (40>y) return "E"
+    else return "Insert Valid Grade!"
+
+
+}
+
+//call out function
+console.log(gradeGenerator(enteredGrade))
